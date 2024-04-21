ec03bf2478af982ccff2ef3e638dd4684e419038
Luuk Eikel <luukekkelenkamp24@gmail.com>
2024-04-21 15:37:38 +0200

    Database connectie up and running
    als je Inteli gebruikt werkt de driver voor de verbinding nu. geen idee hoe je dit in vscode moet doen. online oplossingen werkten tenminste bij mij niet. voor referenties van het maken van queries: zie main bestand

--- hmi/src/Main.java
+++ hmi/src/Main.java
@@ -1,5 +1,21 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/nerdygadgets";
+        String user = "root";
+        String password = "";
 
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("select * from countries")) {
+
+            if (rs.next()) {
+                String kaas = rs.getString("CountryName");
+                System.out.println("Kaas: " + kaas);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
