1ca736a9b2f3cb8ee8de835536288c83a8151aff
Luuk Eikel <luukekkelenkamp24@gmail.com>
2024-04-22 13:28:52 +0200

    Basis functies gemaakt en verwerkt in database functie
    zie titel

--- hmi/src/Main.java
+++ hmi/src/Main.java
@@ -2,20 +2,7 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/nerdygadgets";
-        String user = "root";
-        String password = "";
-
-        try (Connection conn = DriverManager.getConnection(url, user, password);
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("select * from countries")) {
-
-            if (rs.next()) {
-                String kaas = rs.getString("CountryName");
-                System.out.println("Kaas: " + kaas);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        Database db = new Database();
+        db.allOrders();
     }
 }
