ef705e0f2dbb7e439a26f36b13f197d1d6e0e853
IvanDraijer <imdrayer@gmail.com>
2024-04-24 14:58:04 +0200

    comments tweede arduino

--- magazijnrobot_arduino/tweede_arduino/tweede_arduino.ino
+++ magazijnrobot_arduino/tweede_arduino/tweede_arduino.ino
@@ -16,11 +16,11 @@ void loop() {
   //   Serial.print("Ontvangen: ");
   //   Serial.println(received);
   
-    while (Serial.available() > 0) {
+    while (Serial.available() > 0) {    // het ontvangen van het singnaal om naar voren te gaan
     char c = Serial.read();  // Lees één karakter
     if(c == 'g'){
       if(voor){
-        digitalWrite(12,HIGH);
+        digitalWrite(12,HIGH);    // checken of de zas vooruit staat
         voor = false ;
       }
       else{
@@ -29,7 +29,7 @@ void loop() {
       }
       Serial.println(c);
       
-      analogWrite(3, 100);     
+      analogWrite(3, 100);     // het aan en uit zetten van de z as 
       delay(3000);
       analogWrite(3, 0);
       c= 'h';
