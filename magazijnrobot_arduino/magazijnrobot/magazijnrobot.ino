d874dab6dbc56f007fd6caf6278b38da89b5a670
IvanDraijer <imdrayer@gmail.com>
2024-05-07 15:08:22 +0200

    encoders in beide arduinos

--- magazijnrobot_arduino/magazijnrobot/magazijnrobot.ino
+++ magazijnrobot_arduino/magazijnrobot/magazijnrobot.ino
@@ -2,6 +2,13 @@
 int pwmPinLinksRechts = 11;
 int directionPinLinksRechts = 13;
 
+int ENCA = 2;
+int ENCB =7;
+
+int pos = 0;
+long prevT= 0;
+float eprev = 0;
+float  eintergral = 0;
 
 int VRX_PIN =  A2; // Arduino pin connected to VRX pin
 // Arduino pin connected to VRY pin
@@ -12,6 +19,11 @@ bool laasteknopstatus= false;
 
 
 void setup() {
+
+  pinMode(ENCA,INPUT);
+  pinMode(ENCB,INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(ENCA),leesEncoder,RISING);
   
   pinMode(pwmPinLinksRechts, OUTPUT);
   pinMode(directionPinLinksRechts, OUTPUT);
@@ -24,9 +36,11 @@ void setup() {
 }
 
 void loop() {
-  uitlezenJoystick(); // leest joystick uit en beweeegt hiermee de robot
-  //eenmaalknopindrukken();
-  eenmaalknopindrukken();
+  // uitlezenJoystick(); // leest joystick uit en beweeegt hiermee de robot
+  
+  // eenmaalknopindrukken();
+
+  naarbestemming(-1200);
   
 }
 
@@ -97,3 +111,71 @@ void naarRechts(int pwm){
   digitalWrite(directionPinLinksRechts, HIGH);
   analogWrite(pwmPinLinksRechts, pwm);
 }
+
+void naarbestemming(int target){
+ float kp = 1; //PID algoritme variabelen
+ float kd= 0.025;
+ float ki= 0;
+
+ long currentTime = micros();
+
+ float deltaT = ((float)(currentTime - prevT))/ 1.0e6;
+ prevT = currentTime;
+
+ int e = pos- target;
+
+ float dedt = (e-eprev)/ (deltaT);
+ 
+ eintergral = eintergral = e*deltaT;
+
+ float u = kp*e + kd*dedt + ki*eintergral;
+
+ float pwr = fabs(u);
+  if( pwr > 255 ){
+    pwr = 255;
+  }
+  if(pwr < 90 && pwr >0){
+    pwr= 90;
+  }
+
+  // motor direction
+  int dir = 1;
+  if(u<0){
+    dir = 0;
+  }
+
+  // signal the motor
+  setMotor(dir,pwr,pwmPinLinksRechts,directionPinLinksRechts);
+
+
+  // store previous error
+  eprev = e;
+
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.print(pos);
+  Serial.println();
+
+
+}
+
+void leesEncoder(){
+  int b = digitalRead(ENCB);
+  //Serial.println(b);
+  if(b>0){
+    pos++;
+    
+  }
+  else{
+    pos--;
+
+  }
+}
+
+
+void setMotor(int dir, int pwm, int pwmpin, int dirpin){
+  digitalWrite(dirpin, dir);
+  analogWrite(pwmpin, pwm);
+  }
+
+
--- magazijnrobot_arduino/tweede_arduino/tweede_arduino.ino
+++ magazijnrobot_arduino/tweede_arduino/tweede_arduino.ino
@@ -4,13 +4,28 @@ int VRY_PIN = A3;
 int pwmPinBovenOnder = 11;
 int directionPinBovenOnder = 13;
 
+int ENCA = 2;
+int ENCB = 7;
+
+int pos = 0;
+long prevT= 0;
+float eprev = 0;
+float  eintergral = 0;
+
+
 
 void setup() {
   Serial.begin(9600); // Start de seriële communicatie op 9600 baud
+  pinMode(ENCA,INPUT);
+  pinMode(ENCB,INPUT_PULLUP);
+
   pinMode(3, OUTPUT);
   pinMode(12, OUTPUT);
+
   pinMode(pwmPinBovenOnder, OUTPUT);
   pinMode(directionPinBovenOnder, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(ENCA),leesEncoder,RISING);
 
   
 }
@@ -41,11 +56,14 @@ void uitlezenJoystick(){
 } 
 
 void loop() {
+
+  naarbestemming(1200);
   // if (Serial.available() > 0) {
   //   String received = Serial.readString();
   //   Serial.print("Ontvangen: ");
   //   Serial.println(received);
-    uitlezenJoystick();
+    //uitlezenJoystick();
+
   
   //   while (Serial.available() > 0) {    // het ontvangen van het singnaal om naar voren te gaan
   //   char c = Serial.read();  // Lees één karakter
@@ -80,6 +98,70 @@ void naarBoven(int pwm){
 void naarBeneden(int pwm){
   digitalWrite(directionPinBovenOnder, HIGH);
   analogWrite(pwmPinBovenOnder, pwm);
+}
+
+void naarbestemming(int target){
+ float kp = 1; //PID algoritme variabelen
+ float kd= 0.025;
+ float ki= 0;
+
+ long currentTime = micros();
+
+ float deltaT = ((float)(currentTime - prevT))/ 1.0e6;
+ prevT = currentTime;
+
+ int e = pos- target;
+
+ float dedt = (e-eprev)/ (deltaT);
+ 
+ eintergral = eintergral = e*deltaT;
+
+ float u = kp*e + kd*dedt + ki*eintergral;
+
+ float pwr = fabs(u);
+  if( pwr > 255 ){
+    pwr = 255;
+  }
+  if(pwr < 90 && pwr >0){
+    pwr= 90;
+  }
+
+  // motor direction
+  int dir = 1;
+  if(u<0){
+    dir = 0;
+  }
+
+  // signal the motor
+  setMotor(dir,pwr,pwmPinBovenOnder,directionPinBovenOnder);
+
+
+  // store previous error
+  eprev = e;
+
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.print(pos);
+  Serial.println();
+
+
+}
+void leesEncoder(){
+  int b = digitalRead(ENCB);
+  //Serial.println(b);
+  if(b>0){
+    pos++;
+  }
+  else{
+    pos--;
+
+  }
+}
+
+
+void setMotor(int dir, int pwm, int pwmpin, int dirpin){
+  digitalWrite(dirpin, dir);
+  analogWrite(pwmpin, pwm);
 }
 
 
