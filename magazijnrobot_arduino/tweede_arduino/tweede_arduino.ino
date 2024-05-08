695da86826a2a4b55b79a3401c71f41b9848e8af
tristanzwart <63745203+tristanzwart@users.noreply.github.com>
2024-05-08 16:02:32 +0200

    Fixed renamed var

--- magazijnrobot_arduino/tweede_arduino/tweede_arduino.ino
+++ magazijnrobot_arduino/tweede_arduino/tweede_arduino.ino
@@ -209,7 +209,7 @@ void checkEindebaan(){
 
 int getVorkAfstand(){
   // 5v
-  float volts = analogRead(sensor)*0.0048828125;  // value from sensor * (5/1024)
+  float volts = analogRead(irsensor)*0.0048828125;  // value from sensor * (5/1024)
   int distance = 13*pow(volts, -1); // worked out from datasheet graph
   delay(1000); // slow down serial port 
   
