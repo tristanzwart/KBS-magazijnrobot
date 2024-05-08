d0dd8f0597a7d78f1a021bd6a3c9af0f033cfca8
tristanzwart <63745203+tristanzwart@users.noreply.github.com>
2024-05-08 13:45:33 +0200

    Functie voor afstand van de vork opvragen

--- magazijnrobot_arduino/tweede_arduino/tweede_arduino.ino
+++ magazijnrobot_arduino/tweede_arduino/tweede_arduino.ino
@@ -1,6 +1,8 @@
 #define swboven 12
 #define swonder 6
 
+#define irsensor A5
+
 bool voor = false;
 int VRY_PIN = A3;
 
@@ -197,3 +199,14 @@ void checkEindebaan(){
   }
 }
 
+
+int getVorkAfstand(){
+  // 5v
+  float volts = analogRead(sensor)*0.0048828125;  // value from sensor * (5/1024)
+  int distance = 13*pow(volts, -1); // worked out from datasheet graph
+  delay(1000); // slow down serial port 
+  
+  
+  return(distance);   // return de afstand in cm
+  
+}
