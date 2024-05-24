a2873288e6e61d3e2d25a0d383c47dc9a8a67e59
IvanDraijer <imdrayer@gmail.com>
2024-05-24 11:06:31 +0200

    Update magazijnrobot.ino

--- magazijnrobot_arduino/magazijnrobot/magazijnrobot.ino
+++ magazijnrobot_arduino/magazijnrobot/magazijnrobot.ino
@@ -47,8 +47,8 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(digitalRead(handmatigecom));
-  //communicatieHMI();
+  //Serial.println(digitalRead(handmatigecom));
+  communicatieHMI();
   if(!digitalRead(comnoodstop)){
   stop();
   }
@@ -91,7 +91,7 @@ void loop() {
 void uitlezenJoystick(){
 
   int xValue = analogRead(VRX_PIN);
-  Serial.println(xValue);
+  //Serial.println(xValue);
 
   if(xValue < 500){
     stop();
